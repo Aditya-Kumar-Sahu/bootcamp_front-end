cea0e3acacbc44561adee19343ec8bf9b0e18953
Aditya Kumar Sahu <99242571+Aditya-Kumar-Sahu@users.noreply.github.com>
2022-09-04 23:47:15 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return(
+    <div class="note">
+      <h1>This is React Bootcamp</h1>
+      <p>Front-End Web Development. We are learning HTML, CSS, Javascript. Currently learning react.js</p>
+    </div>
+  );
+}
+
+export default Note;
